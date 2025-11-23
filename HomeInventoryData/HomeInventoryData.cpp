2e54a8a3f2fbeeec017de71caf0884d1f6a4dbd8
David-Mile <davide.miglio@outlook.it>
2025-11-23 18:37:57 +0100

    Adds HomeInventoryData library project
    Adds HomeInventoryData, to encapsulate database related logic.
    Rearrange file location in HomeInventoryGUI, ading Ui, Src and Resources folders.
    Updates .gitignore to ignore x64 build output directories.

---
+++ HomeInventoryData/HomeInventoryData.cpp
@@ -0,0 +1,5 @@
+#include "HomeInventoryData.h"
+
+HomeInventoryData::HomeInventoryData()
+{
+}
---
+++ HomeInventoryData/HomeInventoryData.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "homeinventorydata_global.h"
+
+class HOMEINVENTORYDATA_EXPORT HomeInventoryData
+{
+public:
+    HomeInventoryData();
+};
